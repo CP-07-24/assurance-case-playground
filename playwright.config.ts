75e5d1f9e6f498103b8121e98c27c02718cdfe10
hifahir <putratertuahirzi@gmail.com>
2025-06-03 11:37:00 +0700

    update change

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  workers: 50, // Atau "max" untuk maksimal jumlah worker
+  workers: process.env.CI ? 2 : undefined, // Atau "max" untuk maksimal jumlah worker
   timeout: 10 * 60 * 1000, // 10 menit dalam ms
   use: {
     headless: true,
