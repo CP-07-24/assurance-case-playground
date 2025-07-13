4f21a79eb86e10316e256b4d21a6af5aca51476b
hifahir <putratertuahirzi@gmail.com>
2025-07-14 02:27:13 +0700

    Delete for test

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  workers: process.env.CI ? 2 : undefined, // Atau "max" untuk maksimal jumlah worker
-  timeout: 10 * 60 * 1000, // 10 menit dalam ms
-  use: {
-    headless: true,
-  },
-});
